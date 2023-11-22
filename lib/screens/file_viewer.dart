ec37883210a28ed6c5f93648263be99671a6a05b
clairemahon <clairemahon2@gmail.com>
2023-11-22 11:48:19 +0100

    Adding screens - organize file structure

---
+++ lib/screens/file_viewer.dart
@@ -0,0 +1,3 @@
+
+// when a user clicks on a file, this screen is shown
+// it will play back a previously saved workout and chart data 
---
+++ lib/screens/scan_screen.dart
@@ -0,0 +1,4 @@
+
+// Will display the sensor scanner screen
+// Where user can start a new workout (video feed)
+// or view previous workouts (file viewer)
